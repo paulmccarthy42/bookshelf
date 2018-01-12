844c82e29c2b1e6ec27f07a6cb61d1fd322eb487
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-11 22:15:00 -0600

    end of night cleanup

---
+++ public/read.js
@@ -0,0 +1,19 @@
+/* global Vue, VueRouter, axios */
+
+var router = new VueRouter({
+  routes: [{ path: "/:id" }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+});
