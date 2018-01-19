3f6814eb72835467448cf0fe6fb65ac18f37ae56
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-18 19:15:50 -0600

    end to end ocr code complete

--- public/index.js
+++ public/index.js
@@ -249,20 +249,19 @@ var SearchBookPage = {
     uploadFile: function(event) {
       console.log(event.target.files[0]);
       if (event.target.files.length > 0) {
-        this.OCRText = "hi mom!";
-        // var fileCreated = new FormData();
-        // fileCreated.append("image", event.target.files[0]);
-        // axios
-        //   .post("v1/images", fileCreated)
-        //   .then(
-        //     function(response) {
-        //       console.log(response.data.grpc.description);
-        //       this.OCRText = response.data.grpc.description;
-        //     }.bind(this)
-        //   )
-        //   .catch(function(error) {
-        //     console.log("fatal error. please try again");
-        //   });
+        var fileCreated = new FormData();
+        fileCreated.append("image", event.target.files[0]);
+        axios
+          .post("v1/images", fileCreated)
+          .then(
+            function(response) {
+              console.log(response.data.grpc.description);
+              this.OCRText = response.data.grpc.description;
+            }.bind(this)
+          )
+          .catch(function(error) {
+            console.log("fatal error. please try again");
+          });
       }
     },
     submitBook: function() {
