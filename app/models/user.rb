d27aef362ba4d785d088b06f90fda9fe98edf928
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-21 11:29:24 -0600

    pull user's last read book

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :comments
+
+  def last_read_book
+    books = []
+    book_shelves.each do |shelf|
+      shelf.book_selections.each {|book| books << book}
+    end
+    books.sort! {|x, y| y.updated_at <=> x.updated_at}
+    return books[0]
+  end
+
+  def as_json
+    {
+      name: name,
+      role: role,
+      email: email,
+      last_read_book: last_read_book
+    }
+  end
 end
